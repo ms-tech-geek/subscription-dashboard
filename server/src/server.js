f90625f2a7305e0280bac944450e88003944d364
ms-tech-geek <thecorporator@gmail.com>
2025-01-31 07:29:34 +0530

    run server on 8080 port

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const subscriptionRoutes = require('./routes/subscriptionRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
